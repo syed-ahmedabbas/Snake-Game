7fed91c5de6d8e4dab6dc4f25a6bca21749a4765
ahmed_xD <syedahmedabbas.1417@gmail.com>
2026-04-20 22:38:58 +0500

    feat: update snake game logic and add EOD report

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@ using namespace std;
 
     double lastUpdateTime = 0.0; //controls the speed of the snake, we will update the snake's position every 0.1 seconds
     
+    bool ElementInDeque( Vector2 element ,deque<Vector2> deque){
+        for(unsigned int i = 0; i < deque.size(); i++){
+            if(Vector2Equals(deque[i], element)){ //checks if the element is equal to any of the elements in the deque using the Vector2Equals function from raymath.h
+                return true;
+            }
+        }        return false;
+    }
+
     bool eventTriggered(double interval){
         double currentTime = GetTime(); //returns the time in seconds since the program started
         if(currentTime - lastUpdateTime >= interval){
@@ -24,6 +32,7 @@ class Snake{
     public:
     deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}}; //deque is a double ended queue it allows us to add and remove elements from both ends of the queue
     Vector2 direction = Vector2{1,0}; //initial direction is right
+    bool addSegment = false; //flag to indicate whether we need to add a new segment to the snake's body
 
     void Draw(){
         for (unsigned int i = 0; i < body.size(); i++){ //unsigned int to avoid warning about comparing signed and unsigned ints
@@ -36,11 +45,14 @@ class Snake{
     }
 }
     void Update(){
-        //movement
+        //growing length
+         body.push_front(Vector2Add(body[0], direction)); //adds new head to the front of the deque in the direction the snake is moving
+        if(addSegment == true){
+            addSegment = false;
+        }
+        else {
         body.pop_back();
-        body.push_front(Vector2Add(body[0], direction)); //adds new head and removes tail for movement
-
-
+        }
 
         //collision
     }
@@ -50,19 +62,20 @@ class Snake{
 
 
 
-class food{
+class Food{
     public:
-    Vector2 pos = GenerateRandomPos();
+    Vector2 pos;
     Texture2D texture; 
 
-    food(){
+    Food(deque<Vector2> snakeBody ){
         Image image = LoadImage("graphics/food.png"); // Load image from file into CPU memory (RAM);
         ImageResize(&image, 20, 20);
         texture = LoadTextureFromImage(image);
         UnloadImage(image);
+        pos = GenerateRandomPos(snakeBody);
     }
 
-    ~food(){
+    ~Food(){
         UnloadTexture(texture);
     }
 
@@ -70,10 +83,46 @@ class food{
         DrawTexture(texture, pos.x * cSize, pos.y * cSize, WHITE); 
 
     }
-    Vector2 GenerateRandomPos(){
+    
+    Vector2 GenerateRandomCell(){
         float x = GetRandomValue(0, cCount-1);
         float y = GetRandomValue(0, cCount-1);
         return Vector2{x,y};
+    }
+
+
+
+
+    Vector2 GenerateRandomPos(deque<Vector2> snakeBody){ //generate random position for the food, we will pass the snake's body as an argument to this function to ensure that the food does not spawn on top of the snake
+        
+        Vector2 pos = GenerateRandomCell();
+        while(ElementInDeque(pos, snakeBody)){
+            pos = GenerateRandomCell();
+        }
+        return pos;
+    }
+
+ };
+
+ class Game{
+public:
+
+    Snake snake = Snake();
+    Food food = Food(snake.body);
+    
+    void Draw(){
+        food.Draw();
+        snake.Draw();
+    }
+    void Update(){
+        snake.Update();
+        CheckCollisionWithFood();
+    }
+    void CheckCollisionWithFood(){
+        if(Vector2Equals(snake.body[0], food.pos)){ //add new segment to the end of the snake's body
+            food.pos = food.GenerateRandomPos(snake.body); //generate new position for the food
+            snake.addSegment = true;
+        }
     }
  };
 
@@ -81,37 +130,34 @@ int main(){
 
     InitWindow(cSize * cCount, cSize * cCount, "Snake Game"); // opens window
     SetTargetFPS(60); 
-    food Food = food();
-    Snake snake = Snake();
-    
+    Game game = Game();
 
     while(WindowShouldClose()==false){ //game loop
 
         //event handling
 
         //position
-        if(IsKeyPressed(KEY_UP) && snake.direction.y != 1){ //not moving opposite direction at the same time 
-            snake.direction = Vector2{0, -1};
+        if(IsKeyPressed(KEY_UP) && game.snake.direction.y != 1){ //not moving opposite direction at the same time 
+            game.snake.direction = Vector2{0, -1};
         }
-        else if(IsKeyPressed(KEY_DOWN) && snake.direction.y != -1){
-            snake.direction = Vector2{0, 1};
+        else if(IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1){
+            game.snake.direction = Vector2{0, 1};
         }
-        else if(IsKeyPressed(KEY_LEFT) && snake.direction.x != 1){
-            snake.direction = Vector2{-1, 0};
+        else if(IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1){
+            game.snake.direction = Vector2{-1, 0};
         }
-        else if(IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1){
-            snake.direction = Vector2{1, 0};
+        else if(IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1){
+            game.snake.direction = Vector2{1, 0};
         }
         //Draw
         BeginDrawing(); //start of drawing
 
         if(eventTriggered(0.2)){ //update the snake's position every 0.2 seconds
-            snake.Update();
+            game.Update();
         }
         ClearBackground(bg); //background color
 
-        Food.Draw(); //calling food
-        snake.Draw(); //calling snake
+        game.Draw(); //calling game
 
         EndDrawing(); //end of drawing
 
